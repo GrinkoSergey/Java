3922dd660c12ef45349b6007e8ba225bb96d3858
Sergey_Grinko <167552805+GrinkoSergey@users.noreply.github.com>
2024-05-12 12:38:57 +0200

    Delete 15_EntwurfsMuster_Factory/auf02_factory_produkt_sv01 directory

--- 15_EntwurfsMuster_Factory/auf02_factory_produkt_sv01/Produkt/Kleidung.java
+++
@@ -1,8 +0,0 @@
-package aufgaben.auf02_factory_produkt_sv01.Produkt;
-
-public class Kleidung extends Produkt {
-    @Override
-    public void displayInfo() {
-        System.out.println("Hier ist ein Produkt von dem Typ: " + this.getClass().getSimpleName());
-    }
-}
--- 15_EntwurfsMuster_Factory/auf02_factory_produkt_sv01/Produkt/Produkt.java
+++
@@ -1,5 +0,0 @@
-package aufgaben.auf02_factory_produkt_sv01.Produkt;
-
-public abstract class Produkt {
-    public abstract void displayInfo();
-}
