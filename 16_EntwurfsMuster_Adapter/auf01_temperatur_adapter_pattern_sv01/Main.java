c142067c25494578b09843c7154dc7bd9f26b881
Sergey_Grinko <167552805+GrinkoSergey@users.noreply.github.com>
2024-05-14 08:25:52 +0200

    Delete 16_EntwurfsMuster_Adapter/auf01_temperatur_adapter_pattern_sv01 directory

--- 16_EntwurfsMuster_Adapter/auf01_temperatur_adapter_pattern_sv01/Main.java
+++
@@ -1,8 +0,0 @@
-package aufgaben.auf01_temperatur_adapter_pattern_sv01;
-
-public class Main {
-    public static void main(String[] args) {
-        TemperaturSensorCelsius adapter = new Adapter();
-        adapter.ausgabeInCelsius();
-    }
-}
--- 16_EntwurfsMuster_Adapter/auf01_temperatur_adapter_pattern_sv01/TemperaturSensorCelsius.java
+++
@@ -1,5 +0,0 @@
-package aufgaben.auf01_temperatur_adapter_pattern_sv01;
-
-public interface TemperaturSensorCelsius {
-    public void ausgabeInCelsius();
-}
