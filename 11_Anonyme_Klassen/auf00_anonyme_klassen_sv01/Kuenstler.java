a82a230da44b7fec972838d708fa1e6388d32b14
Sergey_Grinko <167552805+GrinkoSergey@users.noreply.github.com>
2024-05-15 16:50:39 +0200

    Delete 11_Anonyme_Klassen directory

--- 11_Anonyme_Klassen/auf00_anonyme_klassen_sv01/Kuenstler.java
+++
@@ -1,6 +0,0 @@
-package aufgaben.auf00_anonyme_klassen_sv01;
-
-// abstrakte Klasse "Künstler" mit einer abstrakten Methode "kunstErstellen()"
-public abstract class Kuenstler {
-    abstract void kunstErstellen();
-}
--- 11_Anonyme_Klassen/auf01_anonyme_klassen_sv01/Operation.java
+++
@@ -1,5 +0,0 @@
-package aufgaben.auf01_anonyme_klassen_sv01;
-// ein Interface mit der Methode 'execute(int a, int b)'
-public interface Operation {
-    public abstract double execute(int a, int b);
-}
