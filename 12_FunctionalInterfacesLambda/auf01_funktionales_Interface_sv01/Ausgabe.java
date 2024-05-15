da472998a941f8b48c4136662761eb3e7d1c621d
Sergey_Grinko <167552805+GrinkoSergey@users.noreply.github.com>
2024-05-15 16:50:48 +0200

    Delete 12_FunctionalInterfacesLambda directory

--- 12_FunctionalInterfacesLambda/auf01_funktionales_Interface_sv01/Ausgabe.java
+++
@@ -1,7 +0,0 @@
-package aufgaben.auf01_funktionales_Interface_sv01;
-
-// das funktionale Interface "Ausgabe" mit der void "ausgeben(String[] daten)"-Methode
-@FunctionalInterface
-public interface Ausgabe {
-    void ausgeben(String[] daten);
-}
