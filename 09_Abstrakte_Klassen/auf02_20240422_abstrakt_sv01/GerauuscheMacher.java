9b24f85e1adc89a8977732c19e57b941b82bb564
Sergey_Grinko <167552805+GrinkoSergey@users.noreply.github.com>
2024-05-15 16:50:23 +0200

    Delete 09_Abstrakte_Klassen directory

--- 09_Abstrakte_Klassen/auf02_20240422_abstrakt_sv01/GerauuscheMacher.java
+++
@@ -1,6 +0,0 @@
-package aufgaben.auf02_20240422_abstrakt_sv01;
-// eine abstrakte Klasse 'GeräuscheMacher'
-public abstract class GerauuscheMacher {
-    // eine abstrakte Methode 'macheGeräusch' (keine Über-, oder Rückgabewerte)
-    public abstract void macheGerauusch();
-}
